9f15a68bb3735cc2f9c404a4ea7158aed7990013
Arnav <arnavprajapati3101@gmail.com>
2026-03-11 16:02:56 +0530

    basic routing done

--- myfirstproject/routes/web.php
+++ myfirstproject/routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "hello duniya kaise ho";
+});
+
+Route::get('/display', function () {
+    for($i=0;$i<=5;$i++){
+        // return $i;
+        echo $i . "<br>" ;
+    }
+});
+
+Route::get('/array', function () {
+    $arr = ["php", "laravel", "javascript", "python"];
+    foreach ($arr as $value) {
+        echo $value . "<br>";
+    }
 });
