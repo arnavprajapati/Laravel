d17d5338bfd2579c08ccb2dcbd3a13ae57733169
Arnav <arnavprajapati3101@gmail.com>
2026-03-11 16:48:42 +0530

    add laravel routing examples

--- myfirstproject/routes/web.php
+++ myfirstproject/routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return "hello duniya kaise ho";
 });
 
+// Basic Routing
 Route::get('/display', function () {
     for($i=0;$i<=5;$i++){
         // return $i;
@@ -18,4 +19,54 @@ Route::get('/array', function () {
     foreach ($arr as $value) {
         echo $value . "<br>";
     }
+});
+
+// Required Routing 
+Route::get('/user/{id}', function ($id) {
+    return "User id ->: " . $id;
+});
+
+//Optional Routing 
+Route::get('/user/{name?}', function ($name = "kuch nhi hai") {
+    return "User name ->: " . $name;
+});
+
+//MultiValue Routing
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return "User id ->: " . $id . " and name ->: " . $name;
+});
+
+//Contraint Routing with numbers
+Route::get('/user/{id}', function ($id) {
+    return "User id ->: " . $id ;
+}) -> where('id', '[0-9]+');
+
+//Contraint Routing with alphabet
+Route::get('/user/{name}', function ($name) {
+    return "User name ->: " . $name ;
+}) -> where('name', '[A-Z]+');
+
+//Contraint Routing with condition
+Route::get('/user/{id}', function ($id){
+    return "The details are " . $id;
+}) -> where('id', '[0-9]{3}');
+
+// Fallback Routing
+Route::fallback(function () {
+    return view('error');
+});
+
+//Named Routing
+Route::get('/user/profile', function () {
+    return "This is user profile page";
+}) -> name('profile');
+
+//Group Routing
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "This is admin dashboard";
+    });
+    Route::get('/settings', function () {
+        return "This is admin settings";
+    });
 });
