4453ccec030e0ebbae8f5e3ca09f91b6ae2bbe91
Arnav <arnavprajapati3101@gmail.com>
2026-03-20 10:39:56 +0530

    first controller and route creation

---
+++ unit-3/app/Http/Controllers/firstcontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class firstcontroller extends Controller
+{
+    public function show() {
+        return "First controller create";
+    }
+}
--- unit-3/routes/web.php
+++ unit-3/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// unit-3 controller, blades and advanced routing
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// controller and route creation 
+
+use App\Http\Controllers\firstcontroller;
+Route::get('/first', [firstcontroller::class, 'show']);
