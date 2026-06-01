f67a904412dd97dd2304168429ae8d76795532d6
Arnav <arnavprajapati3101@gmail.com>
2026-06-01 23:31:26 +0530

    2.Forms & Form Submission

--- laravel/app/Http/Controllers/FormController.php
+++ laravel/app/Http/Controllers/FormController.php
@@ -13,7 +13,19 @@ class FormController extends Controller
 
     public function submitForm(Request $request)
     {
+
+        $request->validate([
+            'name'  => 'required|alpha|min:3',
+            'email' => 'required|email',
+            'phone' => 'required|numeric'
+        ]);
+
         $data = $request->all();
+
+        $name = $data['name'] ?? "missing";
+        $email = $data['email'] ?? "missing";
+        $phone = $data['phone'] ?? "missing";
+
         return
             "Name: " . $data['name'] .
             "<br>Email: " . $data['email'] .
--- laravel/resources/views/MyFormView.blade.php
+++ laravel/resources/views/MyFormView.blade.php
@@ -1,11 +1,36 @@
-<form action="/submit" method="POST">
-    @csrf
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Form Submission</title>
+</head>
+<body>
+    <h2>User Registration Form</h2>
 
-    <input type="text" name="name" placeholder="Name">
+    {{-- Error Block: Displays validation errors if the user makes a mistake --}}
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-    <input type="email" name="email" placeholder="Email">
+    {{-- Form Block: The action must match the Route::post URL --}}
+    <form action="/submit" method="POST">
+        @csrf
+        
+        <label>Name:</label>
+        <input type="text" name="name" placeholder="Enter name"><br><br>
 
-    <input type="text" name="phone" placeholder="Phone">
+        <label>Email:</label>
+        <input type="email" name="email" placeholder="Enter email"><br><br>
 
-    <button type="submit">Submit</button>
-</form>
+        <label>Phone:</label>
+        <input type="text" name="phone" placeholder="Enter phone"><br><br>
+
+        <button type="submit">Submit Form</button>
+    </form>
+</body>
+</html>
