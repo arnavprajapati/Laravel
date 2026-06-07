2b0054b5d0cdf0a3fdd344b9bb61af051c9bbd98
Arnav <arnavprajapati3101@gmail.com>
2026-06-07 22:29:30 +0530

    basic json return

--- lastt/routes/web.php
+++ lastt/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function Pest\Laravel\json;
+
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => 'arnav',
+    ]);
 });
